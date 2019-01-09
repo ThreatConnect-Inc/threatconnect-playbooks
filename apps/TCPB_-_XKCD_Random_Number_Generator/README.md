c4931a87fb42dce75a57be3ff0b6237bcd61a20e
Floyd Hightower <floyd.hightower27@gmail.com>
2019-01-09 18:43:43 -0500

    Adding a couple more random number generator apps

---
+++ apps/TCPB_-_XKCD_Random_Number_Generator/README.md
@@ -0,0 +1,13 @@
+# XKCD Random Number Generator
+
+Create a random number based on the method described here: [https://xkcd.com/221/](https://xkcd.com/221/).
+
+## Release Notes
+
+### 0.0.1
+
+* Initial Release
+
+## P.S.
+
+This is a joke. Don't actually use the app if you want real random numbers.
---
+++ apps/TCPB_-_XKCD_Random_Number_Generator/app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+""" ThreatConnect Playbook App """
+
+# Import default Playbook Class (Required)
+from playbook_app import PlaybookApp
+
+
+class App(PlaybookApp):
+    """ Playbook App """
+
+    def run(self):
+        """  Run the App main logic.
+
+        This method should contain the core logic of the App.
+        """
+        self.tcex.playbook.create_output('xkcdRandom.integer', '4', 'String')
+
+        # set the App exit message
+        self.exit_message = 'Random number created'
