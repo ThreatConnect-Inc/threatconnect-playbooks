fa3f4abcb62b31149c73d0703a778533c197c4c6
Floyd Hightower <floyd.hightower27@gmail.com>
2019-01-10 09:15:45 -0500

    Adding app to rotate a string
    Fixes #103

---
+++ apps/TCPB_-_String_Rotate/args.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+""" Auto-generated Playbook Args """
+
+
+class Args(object):
+    """ Playbook Args """
+
+    def __init__(self, _tcex):
+        """ Initialize class properties. """
+        _tcex.parser.add_argument('--string')
+        _tcex.parser.add_argument('--rotation')
